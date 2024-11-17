a14486f426b849f9d813f264469cbbcd2b90606c
ramirosanes <ramirosanes@gmail.com>
2024-11-17 14:41:15 -0300

    apenas funciona

--- server.c
+++ server.c
@@ -14,52 +14,87 @@
 // Función para procesar los comandos
 void process_command(int client_fd, const char *command) 
 {
-    char buffer[MAX_BUFFER*2] = {0}; //No me funcionaba si no ponia este x2 y no entiendo porq
-    char value[MAX_BUFFER] = {0};
-    char key[MAX_BUFFER] = {0};  
+    char buffer[MAX_BUFFER] = {0}; //acá se guardan las respuestas para el cliente
+    char *token; 
     FILE *file;
 
-    //Recibo comando
-    if (sscanf(command, "SET %s %s", key, value) == 2) 
-    {
-        file = fopen(key, "w");
-        if (file) 
-        {
-            fprintf(file, "%s", value);
-            fclose(file);
-            snprintf(buffer, sizeof(buffer), "OK\n");
-        } else {
-            perror("Error al abrir archivo\n");
-        }
-    } else if (sscanf(command, "GET %s", key) == 1) 
-    {
-        file = fopen(key, "r");
-        if (file) 
-        {
-            fgets(value, sizeof(value), file);
-            fclose(file);
-            snprintf(buffer, sizeof(buffer), "OK\n%s\n", value);
-        } else {
-            if (errno == ENOENT) {
-                snprintf(buffer, sizeof(buffer), "NOTFOUND\n");
+    //Necesito copiar a otro array no const para poder trabajar con strtok
+    char *command_copy = strdup(command);
+    //strtok guarda en token el primer string delimitado por espacios " ", y lo termina con \0
+    token = strtok(command_copy, " ");
+
+    //una vez que guardo el primer string en token, empiezo el procesamiento
+    if (token != NULL) {
+        if (strcmp(token, "SET") == 0) { 
+            // Comando SET
+            // strtok utiliza el argumento utilizado anteriormente si le paso NULL, pero continuando donde estaba antes
+            char *key = strtok(NULL, " ");
+            char *value = strtok(NULL, " ");
+
+            //Tengo guardado en *key y *value los punteros a los strings a procesar
+
+            if (key != NULL && value != NULL) {
+                file = fopen(key, "w"); //Creo un nuevo file con nombre key en modo write
+                if (file) {
+                    fprintf(file, "%s", value);
+                    fclose(file);
+                    snprintf(buffer, sizeof(buffer), "OK\n"); //Guardo en buffer la respuesta a devolverle al cliente
+                } else {
+                    perror("Error opening file\n");
+                }
             } else {
-                perror("Error al leer archivo\n");
+                //Si se utiliza mal el comando llego acá
+                snprintf(buffer, sizeof(buffer), "Invalid SET command format\n");
             }
-        }
-    } else if (sscanf(command, "DEL %s", key) == 1) 
-    {
-        if (remove(key) == 0) {
-            snprintf(buffer, sizeof(buffer), "OK\n");
-        } else {
-            if (errno == ENOENT) {
-                snprintf(buffer, sizeof(buffer), "OK\n");
+
+        } else if (strcmp(token, "GET") == 0) {
+            // Comando GET, misma logica que anterior
+            char *key = strtok(NULL, " ");
+            //Agrego este buffer porque me fallaba el sprintf de 4 argumentos (ni idea porque)
+            char temp_buffer[MAX_BUFFER] = {0}; 
+
+            if (key != NULL) {
+                file = fopen(key, "r"); //Intento abrir un archivo anteriormente creado llamado key
+                if (file) {
+                    fgets(temp_buffer, sizeof(temp_buffer), file); //Leo archivo anteriormente creado y guardo en buffer para responder
+                    fclose(file);
+                    snprintf(buffer, sizeof(buffer)+5, "OK\n%s\n", temp_buffer); //Si no ponia +5 no funcionaba (no me compilaba aunque no hubiera manera de superar el tamaño del buffer pero bueno)
+                } else {
+                    if (errno == ENOENT) {
+                        snprintf(buffer, sizeof(buffer), "NOTFOUND\n"); //Respuesta en caso de que no se encuentre la key
+                    } else {
+                        perror("Error reading file\n");
+                    }
+                }
             } else {
-                perror("Error al remover archivo\n");
+                snprintf(buffer, sizeof(buffer), "Invalid GET command format\n");
             }
+
+        } else if (strcmp(token, "DEL") == 0) {
+            // Comando DEL, misma lógica
+            char *key = strtok(NULL, " ");
+
+            if (key != NULL) {
+                if (remove(key) == 0) {
+                    snprintf(buffer, sizeof(buffer), "OK\n"); //En caso de exito escribimos respuesta
+                } else {
+                    if (errno == ENOENT) {
+                        snprintf(buffer, sizeof(buffer), "Inexistente\n");
+                    } else {
+                        perror("Error removing file\n");
+                    }
+                }
+            } else {
+                snprintf(buffer, sizeof(buffer), "Invalid DEL command format\n");
+            }
+
+        } else {
+            snprintf(buffer, sizeof(buffer), "Unrecognized command\n");
         }
     } else {
-        snprintf(buffer, sizeof(buffer), "Comando no reconocido\n");
+        snprintf(buffer, sizeof(buffer), "Invalid command format\n");
     }
+
     //Escribo en el file del cliente la respuesta
     write(client_fd, buffer, strlen(buffer));
 }
@@ -82,6 +117,8 @@ void handle_client(int client_fd)
   
   // Procesar el comando
   process_command(client_fd, buffer);
+  // Cierro conexion con cliente
+  close(client_fd);
 }
 
 int main(void) {
@@ -139,8 +176,6 @@ int main(void) {
 
         handle_client(client_fd);
     }
-    
-    // Cerramos conexion con cliente
-    close(client_fd);
+
     return EXIT_SUCCESS;
 }
